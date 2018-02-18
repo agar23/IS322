95dc6fcd4f7b3cc567344d30752b6e8ca889a886
aga23 <aga23@njit.edu>
2018-02-18 16:10:30 -0500

    Added basic layout and aligend button

--- TriviaBet/App.js
+++ TriviaBet/App.js
@@ -9,6 +9,13 @@ import {
 
 import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 
+const title = "TriviaBet \nTrivia with Stakes";
+const title2 = "Trivia";
+const title3 = "Bet\n";
+const title4 = "Trivia with ";
+const title5 = "Stakes";
+
+
 export default class App extends Component<Props> {
 
     _callGoogle(){
@@ -29,11 +36,27 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+
         <TouchableOpacity onPress = {this._callGoogle.bind(this)}>
           <View style = {styles.button}>
-            <Text style = {styles.buttonText}> Google Sign In</Text>
-          </View>
+            <Text style = {styles.buttonText}> </Text>
+            </View>
         </TouchableOpacity>
+
+        <GoogleSigninButton
+         style={{width: 180, height: 54, position: 'absolute', bottom:40, left:15}}
+         size={GoogleSigninButton.Size.Standar}
+         color={GoogleSigninButton.Color.Light}
+         onPress={this._callGoogle.bind(this)}
+         />
+
+
+        <Text style={styles.index}>
+          {title2}
+          <Text style={{color: '#f77f07'}}>{title3}</Text>
+          {title4}
+          <Text style={{color: '#f77f07'}}>{title5}</Text>
+        </Text>
       </View>
     );
   }
@@ -44,18 +67,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'steelblue',
   },
-  button: {
-    justifyContent: 'center',
-    alignItems:'center',
-    padding: 10,
-    borderRadius:10,
-    backgroundColor:'rgb(202,0,0)'
+  index: {
+  	fontFamily: 'copperplate',
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+    margin: 10,
+    top: -200,
   },
-  buttonText: {
-    fontSize:16,
-    fontWeight :'bold',
-    color:'#FFFFFF'
-  }
+
 });
