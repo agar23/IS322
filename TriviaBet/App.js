ef13b9467be3c980c033b9477dad2b04930bf525
nicolaslaing <34254569+nicolaslaing@users.noreply.github.com>
2018-03-14 10:15:45 -0400

    Update App.js

--- TriviaBet/App.js
+++ TriviaBet/App.js
@@ -1,15 +1,18 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * TriviaBet
+ * By Alfonso Austin, Thanh Tran, and Nicolas Laing
+ * IS 332 Mobile Application Design
  */
 
 import React, { Component } from 'react';
+//import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity
 } from 'react-native';
 
 const title = "TriviaBet \nTrivia with Stakes";
@@ -23,12 +26,34 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+
+        <View style={{flexDirection: 'row'}}>
+          <Image  style={styles.logo} source={require('./images/logo.png')} />
+        </View>
+
         <Text style={styles.index}>
           {title2}
           <Text style={{color: '#f77f07'}}>{title3}</Text>
           {title4}
           <Text style={{color: '#f77f07'}}>{title5}</Text>
         </Text>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Play</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text2}>Play with Friends</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Options</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Profile</Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
@@ -42,11 +67,35 @@ const styles = StyleSheet.create({
     backgroundColor: 'steelblue',
   },
   index: {
-  	fontFamily: 'copperplate', 
+    fontFamily: 'copperplate', 
     fontSize: 30,
     color: 'white',
     textAlign: 'center',
     margin: 10,
-    top: -200,
+    top: -100,
+  },
+  logo: {
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    top: -100
+  },
+  text: {
+    fontFamily: 'copperplate',
+    fontSize: 30, 
+    color: 'white'
+  },
+  text2: {
+    fontFamily: 'copperplate',
+    fontSize: 20, 
+    color: 'white'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#f77f07',
+    padding: 15,
+    margin: 3,
+    width: 220,
+    height: 60
   }
 });
